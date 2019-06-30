c316b66d918a750ccf9ae66610e3228d4c4e3da4
nsuzuki7713 <nsuzuki7713@gmail.com>
2019-07-01 00:38:46 +0900

    update index

--- pages/index.vue
+++ pages/index.vue
@@ -2,24 +2,69 @@
   <!-- <v-layout column justify-center align-center> -->
   <v-layout row grid-list-md text-xs-center wrap>
     <v-flex xs4>
-      <v-card dark color="secondary">
-        <v-avatar :tile="tile" size="200" color="grey lighten-4">
-          アバター
-          <img
-            src="https://vuetifyjs.com/apple-touch-icon-180x180.png"
-            alt="avatar"
-          />
+      <v-card>
+        <v-avatar :tile="tile" size="200">
+          <img src="~/assets/icon.JPG" alt="avatar" />
         </v-avatar>
       </v-card>
     </v-flex>
     <v-flex xs8>
-      <v-card dark color="secondary">
-        <v-card-text class="px-0">基本情報</v-card-text>
+      <v-card>
+        <v-card-text class="px-0">
+          <h2>
+            基本情報(<a @click="switchBasicInfoType(1)">JSON</a>/<a
+              @click="switchBasicInfoType(2)"
+              >CSV</a
+            >/<a @click="switchBasicInfoType(3)">YAML</a>/<a
+              @click="switchBasicInfoType(4)"
+              >Table</a
+            >)
+          </h2>
+          <pre v-if="basicInfoType !== 4">
+            <code class="text-sm-left" style="font-size:14px;width: 80%;background: white;" v-if="basicInfoType===1">
+              body { 
+                    名前: "なおと",
+                    性別: "男性",
+                    生年月日: "1993年",
+                    生息地: "東京都",
+                    区分: "個人事業主",
+              }
+            </code>
+            <code class="text-sm-left" style="font-size:14px;width: 80%;background: white;" v-if="basicInfoType===2">
+              "名前","性別","生年月日","生息地","区分"
+              "なおと","男性","1993年","東京都","個人事業主"
+            </code>
+            <code class="text-sm-left" style="font-size:14px;width: 80%;background: white;" v-if="basicInfoType===3">
+              body
+                  名前: "なおと"
+                  性別: "男性"
+                  生年月日: "1993年"
+                  生息地: "東京都"
+                  区分: "個人事業主"
+            </code>
+          </pre>
+          <div
+            style="font-size:14px;width: 80%;background: white;margin:0 auto;"
+            v-if="basicInfoType === 4"
+          >
+            <v-data-table :headers="headers" :items="desserts" hide-actions>
+              <template v-slot:items="props">
+                <td class="text-xs-left">{{ props.item.key }}</td>
+                <td class="text-xs-left">{{ props.item.value }}</td>
+              </template>
+            </v-data-table>
+          </div>
+        </v-card-text>
       </v-card>
     </v-flex>
     <v-flex xs3>
       <v-card dark color="secondary">
-        <v-card-text class="px-0">GitHub</v-card-text>
+        <v-card-text class="px-0">
+          <div>GitHub</div>
+          <div>contributions:700</div>
+          <div>直近一週間:700</div>
+          <div>{{ items.results_returned }}</div>
+        </v-card-text>
       </v-card>
     </v-flex>
     <v-flex xs3>
@@ -105,7 +150,43 @@ export default {
     VuetifyLogo
   },
   data: () => ({
-    tile: false
-  })
+    tile: false,
+    basicInfoType: 1,
+    headers: [
+      { text: 'key', value: 'key', sortable: false },
+      { text: 'value', value: 'value', sortable: false }
+    ],
+    desserts: [
+      {
+        key: '名前',
+        value: 'なおと'
+      },
+      {
+        key: '性別',
+        value: '男性'
+      },
+      {
+        key: '生年月日',
+        value: 1993
+      },
+      {
+        key: '生息地',
+        value: '東京'
+      },
+      {
+        key: '区分',
+        value: '個人事業主'
+      }
+    ]
+  }),
+  async asyncData({ app }) {
+    const items = await app.$axios.$get('https://connpass.com/api/v1/event/')
+    return { items }
+  },
+  methods: {
+    switchBasicInfoType(type) {
+      this.basicInfoType = type
+    }
+  }
 }
 </script>
